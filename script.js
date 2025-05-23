9d8883854825ff60de7100bb64b6353a12788a99
Emerson22ed <emersonquadrado5@gmail.com>
2025-05-23 09:23:03 -0300

    novafoto

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("#mensagem")
+
+  button.addEventListener("mouseover", e =>{button.style.color="green";})
+
+    button.addEventListener("mouseout", e =>{button.style.color="blue"});
+
+       function botao(){document.getElementById("mensagem").innerHTML="JESUS TE AMA- joão3:16: porque Deus amou o mundo de tal maneira que deu seu filho unigenito para que todo aquele que nele crer não pereça mas tenha s vida eterna"}
+  
