da509b9d20bf9e6d4bc96adae4c76b4035e07d2d
Emerson22ed <emersonquadrado5@gmail.com>
2025-05-23 22:08:14 -0300

    atualizações no código

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ let button = document.querySelector("#mensagem")
     button.addEventListener("mouseout", e =>{button.style.color="blue"});
 
        function botao(){document.getElementById("mensagem").innerHTML="JESUS TE AMA- joão3:16: porque Deus amou o mundo de tal maneira que deu seu filho unigenito para que todo aquele que nele crer não pereça mas tenha s vida eterna"}
-
+                                                                        
